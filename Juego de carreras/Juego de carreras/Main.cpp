07e577fa21d6403bf08c30edca7b380fb91503c4
N3BUR4G <ruben.alvarez@estudiant.enti.cat>
2025-12-01 10:12:35 +0100

    lanzarDado

--- Juego de carreras/Juego de carreras/Main.cpp
+++ Juego de carreras/Juego de carreras/Main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include "Tablero.h"
+#include <cstdlib>
+#include <ctime>
 
-
+int lanzarDado() {
+	return 1 + rand() % 6;
+}
 int main() {
-
+	srand(time(NULL));
 	char tablero[CASILLAS];
 
+	int resultado = lanzarDado();
+	std::cout << "Has sacado un " << resultado;
 	Tablero(tablero);
 
 	return 0;
