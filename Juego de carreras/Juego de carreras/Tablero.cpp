2de8a19313b7d1ecfbbefb9d565e7f69b98cb974
guillemrull111 <guillem.rull@estudiant.enti.cat>
2025-12-01 10:53:44 +0100

    f

--- Juego de carreras/Juego de carreras/Tablero.cpp
+++ Juego de carreras/Juego de carreras/Tablero.cpp
@@ -3,7 +3,7 @@
 
 void InicializarTablero(char tablero[CASILLAS]) {
     for (int i = 0; i < CASILLAS; i++) {
-        tablero[i] = '.';  
+        tablero[i] = '.';
         std::cout << tablero[i];  
     }
     std::cout << std::endl;  
